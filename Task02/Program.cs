f9d523f27593d888417e9f49a16c0c3fd25655a0
Mikhail <i@MkRusakov.ru>
2022-08-17 20:43:05 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int funct(int num)
+{
+    int b = 0;
+    string str_num = Convert.ToString(num);
+    for (int i = 0; i < str_num.Length; i++)
+    {
+        int a = Convert.ToInt32(str_num[i].ToString());
+        b = b + a;
+    }
+    return b;
+}
+System.Console.WriteLine(funct(55));
