02708c1928fea37a2a745705f98abdc25a6eeb01
Mikhail <i@MkRusakov.ru>
2022-08-17 18:38:07 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+double fun(int a, int b)
+{
+    double res = Math.Pow(a,b);
+    return res;
+}
+System.Console.WriteLine(fun(3,5));
